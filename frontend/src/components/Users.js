f67291ffc21f4d1be51d898de70db7c390352682
tkdeshan <tkdeshan1103@gmail.com>
2023-10-04 23:07:40 +0530

    Complete user component

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Users() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://shopping-list-management-web-app-backend.vercel.app/api/items'
+      )
+      .then((result) => setItems(result.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>Users</div>
-  )
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <Link to="/create" className="btn btn-success">
+          Add +
+        </Link>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Item Number</th>
+              <th>Item Name</th>
+              <th>Quentity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item._id}>
+                <td>{item.itemId}</td>
+                <td>{item.itemName}</td>
+                <td>{item.quentity}</td>
+                <td>
+                  <button>Update</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Users
+export default Users;
