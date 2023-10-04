08aaadca7bb2dbf57fa33762e3014bd605c8bbe0
tkdeshan <tkdeshan1103@gmail.com>
2023-10-04 21:56:53 +0530

    Add route

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const itemRoutes = require('./routes/itemRoutes');
 
 const app = express();
 
 const PORT = 5000;
 
-app.get('/api/get', (req, res) => {
-  res.send('hello world');
-});
+app.use(cors());
+app.use(express.json());
+app.use('/api', itemRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`The server is running http://localhost:${PORT}`);
