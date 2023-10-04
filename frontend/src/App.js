b923e679cad3211a8f7da9bdd61b232410f8eee1
tkdeshan <tkdeshan1103@gmail.com>
2023-10-04 22:46:41 +0530

    Create components and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './components/Users';
+import CreateUser from './components/CreateUser';
+import UpdateUser from './components/UpdateUser';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Users />}></Route>
+          <Route path='/create' element={<CreateUser />}></Route>
+          <Route path='/update/:id' element={<UpdateUser />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
