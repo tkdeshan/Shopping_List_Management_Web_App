b479ff6f7de8b99d269d7d52168ad9aad8c82690
tkdeshan <tkdeshan1103@gmail.com>
2023-10-04 21:16:56 +0530

    Create server

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running http://localhost:${PORT}`);
+});
