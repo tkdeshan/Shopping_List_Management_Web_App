b4f6ab6193ff574ade9186d86cfcb4dd307f6285
tkdeshan <tkdeshan1103@gmail.com>
2023-10-04 22:11:53 +0530

    Create API read data from id

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const Items = require('../modules/itemModel');
 
 router.get('/items', (req, res) => {
-    Items.find({})
+  Items.find({})
+    .then((items) => res.json(items))
+    .catch((err) => res.json(err));
+});
+
+// Read data from id
+router.get('/items/:id', (req, res) => {
+  const id = req.params.id;
+  Items.findById({ _id: id })
     .then((items) => res.json(items))
     .catch((err) => res.json(err));
 });
